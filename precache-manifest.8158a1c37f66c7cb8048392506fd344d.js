3a42f4ee8100bf6fe1cb0deafb30545fc448c888
sadeghjalali <s.jalali.tsu@gmail.com>
2019-03-31 11:05:37 +0330

    Updates

---
+++ precache-manifest.8158a1c37f66c7cb8048392506fd344d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8eed06dde3666c48872",
+    "url": "/todoapp/static/js/runtime~main.f8eed06d.js"
+  },
+  {
+    "revision": "cc57d9aaaf02fb569dee",
+    "url": "/todoapp/static/js/main.cc57d9aa.chunk.js"
+  },
+  {
+    "revision": "01cd050e3ded6177afe0",
+    "url": "/todoapp/static/js/2.01cd050e.chunk.js"
+  },
+  {
+    "revision": "cc57d9aaaf02fb569dee",
+    "url": "/todoapp/static/css/main.fb509d25.chunk.css"
+  },
+  {
+    "revision": "48ac1deb0a5ab2f2da7ffaff90fe61e3",
+    "url": "/todoapp/index.html"
+  }
+];
